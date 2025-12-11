130d1837a9ad1ada975091e0c47d29e3e615b092
Aayush3003265 <aayushadhikari70@gmail.com>
2025-12-11 17:01:54 +0545

    collestion list

---
+++ day-5/collection.py
@@ -0,0 +1,26 @@
+# collection = single "variable" used to store multiple values
+# List = [] ordered and changeable. Dublicates OK
+# Set = {} unordered and immutable, but Add/Remove OK.  No dublicates
+# Tuple = () ordered and unchangeable. Dublicates OK. Faster
+# List: 
+fruits = ["apple","banana","coconut","orange"]
+print(fruits[1]) #banana
+print(fruits[::2]) #['apple', 'coconut']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
